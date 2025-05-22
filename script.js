60265c9a371aaa4022649c7f7810def5cea20636
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-22 19:06:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+document.getElementById("submit").addEventListener("click",()=>{
+	const name= document.getElementId('username').value.trim();
+	const password = document.getElementId('password').value;
+	
+	alert(`Logged in as ${name}` );
+})
