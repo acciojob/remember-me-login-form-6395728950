f1a2ccb7bc14d75c18b52683c1718c5dc1c0a802
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-05-23 13:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 //your JS code here. If required.
  
 
-document.getElementById("submit").addEventListener("click", (e) => {
-    e.preventDefault();
+document.getElementById("submit").addEventListener("click",(e)=>{
+e.preventDefault();
+ 	document.getElementById('existing').style.display="none";
 
-    const name = document.getElementById('username').value.trim();
-    const password = document.getElementById('password').value;
-    const remember = document.getElementById('checkbox');
-	  alert(`Logged in as ${name}`);
+const name= document.getElementById('username').value.trim();
 
-    // Check if there are existing credentials
-    if (localStorage.getItem("name") && localStorage.getItem("password")) {
-        document.getElementById('existing').style.display = "block";
-        document.getElementById('existing').addEventListener("click", () => {
-            alert(`Logged in as ${localStorage.getItem("name"}`);
-        });
-    }
-
-    if (remember.checked) {
-        localStorage.setItem("name", name);
-        localStorage.setItem("password", password);
-    } else {
-        localStorage.removeItem("name");
-        localStorage.removeItem("password");
-    }
-
-   
-});
+const password = document.getElementById('password').value;
+const remember = document.getElementById('checkbox');
+if(localStorage.getItem("name")!==null &&  localStorage.getItem("password")!==null){
+	document.getElementById('existing').style.display="block";
+	document.getElementById('existing').addEventListener("click",()=>{
+		alert(`Logged in as ${localStorage.getItem("name")}`);
+	});
 	
+}
+if(remember.checked){
+	localStorage.setItem("name",name);
+	localStorage.setItem("password",password);
+ 
+
+	
+}else{
+	localStorage.removeItem("name");
+	localStorage.removeItem("password");
+		document.getElementById('existing').style.display="none";
+	
+	
+}
+
+alert(`Logged in as ${name}`);
 })
